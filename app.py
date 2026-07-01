e19564719249426bfcbaf3b37dd63a18cac87d84
yskc0315 <ihcikihsoy1503@gmail.com>
2026-07-01 23:02:08 +0900

    Add task list API

--- app.py
+++ app.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello Task Raid!"}
+
+@app.get("/tasks")
+def get_tasks():
+    return [
+        {
+            "id": 1,
+            "title": "FastAPIを学ぶ",
+            "completed": False
+        },
+        {
+            "id": 2,
+            "title": "Task Raidを作る",
+            "completed": True
+        }
+    ]
